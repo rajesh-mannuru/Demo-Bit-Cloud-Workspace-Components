8267293814869557429dc347fc699f844e7a82ea
rajesh-mannuru <rajesh.mannuru@gmail.com>
2023-03-08 08:55:43 -0500

    Initial Workspace and Environment Setup

---
+++ components/extensions/rm-react-env/index.ts
@@ -0,0 +1,5 @@
+import { RmReactEnvAspect } from './rm-react-env.aspect';
+
+export type { RmReactEnvMain } from './rm-react-env.main.runtime';
+export default RmReactEnvAspect;
+export { RmReactEnvAspect };
---
+++ components/extensions/rm-react-env/rm-react-env.aspect.ts
@@ -0,0 +1,5 @@
+import { Aspect } from '@teambit/harmony';
+
+export const RmReactEnvAspect = Aspect.create({
+  id: 'rajesh_mannuru.sandbox-scope/extensions/rm-react-env',
+});
